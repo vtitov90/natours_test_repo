055185c7d78bafe574b2c31f90e6288df922ee79
valerii_titov <valerii.titov@ext.azerion.com>
2026-03-15 23:22:09 +0100

    replaced .catch((err) => next(err)) with .catch(next) so errors are passed to next without an extra wrapper.

--- utils/appError.js
+++ utils/appError.js
@@ -6,7 +6,9 @@ class AppError extends Error {
     this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
     this.isOperational = true;
 
-    Error.captureStackTrace(this, this.constructor);
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, this.constructor);
+    }
   }
 }
 
